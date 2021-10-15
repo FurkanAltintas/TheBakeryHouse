227c9c29e5e47a13e523566358157f8b5760f007
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:56:15 +0300

    Calendar Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/CalendarController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class CalendarController : Controller
+    {
+        ReservationManager reservationManager = new ReservationManager(new EfReservationDal());
+        // GET: Dashboard/Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Events()
+        {
+            return Json(reservationManager.ListCalendar(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
