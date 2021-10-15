260d879172118fd62b89f47806167034e5e9649b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 20:21:11 +0300

    Employee

---
+++ EntityLayer/Concrete/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int AuthorityId { get; set; }
+        [ForeignKey("AuthorityId")]
+        public Authority Authority { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string UserName { get; set; }
+        public virtual string FullName { get => Name + " " + SurName; }
+        public string Profile { get; set; }
+        public string Mobile { get; set; }
+        public string About { get; set; }
+        public string WebSite { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RPassword { get; set; }
+    }
+}
