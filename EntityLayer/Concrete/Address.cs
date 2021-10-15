a673608e5ba6916637083e605fdcc73edfe9d7a5
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 20:20:40 +0300

    Address

---
+++ EntityLayer/Concrete/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Map { get; set; }
+        public string Home { get; set; }
+        public string HomeSubTitle { get; set; }
+        public string Phone { get; set; }
+        public string PhoneSubTitle { get; set; }
+        public string Email { get; set; }
+        public string EmailSubTitle { get; set; }
+    }
+}
