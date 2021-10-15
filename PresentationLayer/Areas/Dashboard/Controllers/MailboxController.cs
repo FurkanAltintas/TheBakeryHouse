b33978d73469140a518822b75d39cd90de958174
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:57:00 +0300

    Mailbox Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/MailboxController.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using PresentationLayer.Areas.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class MailboxController : BaseController
+    {
+        MailManager mailManager = new MailManager(new EfMailDal());
+        // GET: Dashboard/Mailbox
+        public ActionResult Inbox()
+        {
+            var list = mailManager.Inbox(Email).OrderByDescending(x => x.History);
+            return View(list);
+        }
+
+        /* mail/u/0/#starred/urlhash */
+        public ActionResult MailRead(string status, int id)
+        {
+            var list = mailManager.Find(id);
+            return View(list);
+        }
+
+        //Partial
+        public PartialViewResult Compose()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult UnRead()
+        {
+            ViewBag.UnRead = mailManager.Inbox(Email).Where(x => x.IsRead == false).Count();
+            return PartialView();
+        }
+
+    }
+}
