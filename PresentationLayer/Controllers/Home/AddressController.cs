34601cab2e033f577c6f03df16fce2603e46dc24
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 17:33:49 +0300

    Address Controller

---
+++ PresentationLayer/Controllers/Home/AddressController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers.Home
+{
+    public class AddressController : Controller
+    {
+        AddressManager addressManager = new AddressManager(new EfAddressDal());
+        // GET: Address
+        public PartialViewResult Index()
+        {
+            var first = addressManager.First();
+            return PartialView(first);
+        }
+    }
+}
