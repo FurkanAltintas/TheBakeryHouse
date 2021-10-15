7b4137dc0580fc7c5b4d4be99db22baa2e2a5b8a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 17:33:35 +0300

    About Controller

---
+++ PresentationLayer/Controllers/Home/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers.Home
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+        // GET: About
+        public ActionResult Index()
+        {
+            var first = aboutManager.First();
+            return View(first);
+        }
+    }
+}
