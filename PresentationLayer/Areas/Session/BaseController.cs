b1db74ecf77159ac718d7c24b7c5dde1c46160d1
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 20:02:35 +0300

    BaseController

---
+++ PresentationLayer/Areas/Session/BaseController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Session
+{
+    public class BaseController : Controller
+    {
+        EmployeeManager employeeManager = new EmployeeManager(new EfEmployeeDal());
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Image { get; set; }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (Session["Email"] != null)
+            {
+                Email = (string)Session["Email"];
+                var user = employeeManager.Find(Email);
+                Id = user.Id;
+                Name = user.FullName;
+                Image = user.Profile;
+            }
+            else
+            {
+
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
