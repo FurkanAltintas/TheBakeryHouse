dead9d5217e16be9a629551a36a7d3532e99bd04
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:47:10 +0300

    Address Manager

---
+++ BusinessLayer/Concrete/AddressManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AddressManager : IAddressService
+    {
+        IAddressDal _addressDal;
+
+        public AddressManager(IAddressDal addressDal)
+        {
+            _addressDal = addressDal;
+        }
+
+        public void Add(Address p)
+        {
+            _addressDal.Create(p);
+        }
+
+        public void Delete(Address p)
+        {
+            _addressDal.Delete(p);
+        }
+
+        public Address First()
+        {
+            return _addressDal.First();
+        }
+
+        public List<Address> List()
+        {
+            return _addressDal.Read();
+        }
+
+        public void Update(Address p)
+        {
+            _addressDal.Update(p);
+        }
+    }
+}
