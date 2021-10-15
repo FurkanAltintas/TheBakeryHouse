4be2b557c8e3a0547e999740d36cb8419c9c4d76
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 17:34:16 +0300

    Banner Controller

---
+++ PresentationLayer/Controllers/Home/BannerController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers.Home
+{
+    public class BannerController : Controller
+    {
+        BannerManager bannerManager = new BannerManager(new EfBannerDal());
+        // GET: _PartialBanner
+        public PartialViewResult Index()
+        {
+            var first = bannerManager.First();
+            return PartialView(first);
+        }
+    }
+}
