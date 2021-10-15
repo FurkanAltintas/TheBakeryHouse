6cbf16c0207a1aa258bb4795d7d93a47de1bb6cd
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:47:32 +0300

    Currency Manager

---
+++ BusinessLayer/Concrete/CurrencyManager.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CurrencyManager : ICurrencyService
+    {
+        ICurrencyDal _currencyDal;
+
+        public CurrencyManager(ICurrencyDal currencyDal)
+        {
+            _currencyDal = currencyDal;
+        }
+
+        public void Add(Currency p)
+        {
+            _currencyDal.Create(p);
+        }
+
+        public void Delete(Currency p)
+        {
+            _currencyDal.Delete(p);
+        }
+
+        public List<Currency> List()
+        {
+            return _currencyDal.Read();
+        }
+
+        public void Update(Currency p)
+        {
+            _currencyDal.Update(p);
+        }
+    }
+}
