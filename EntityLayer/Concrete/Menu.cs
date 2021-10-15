146674cd8960879a020ff6f17c33d92eda923e8f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 20:21:33 +0300

    Menu

---
+++ EntityLayer/Concrete/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Menu
+    {
+        [Key]
+        public int Id { get; set; }
+        public int? CurrencyId { get; set; }
+        [ForeignKey("CurrencyId")]
+        public Currency Currency { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Image { get; set; }
+        public decimal? Price { get; set; }
+        public bool IsTopRated { get; set; }
+    }
+}
