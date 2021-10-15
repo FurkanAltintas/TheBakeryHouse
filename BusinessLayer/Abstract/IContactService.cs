cf8141de66cb44851d26af85032975dc60f86712
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:45:36 +0300

    Contact Service

---
+++ BusinessLayer/Abstract/IContactService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IContactService
+    {
+        List<Contact> List();
+        void Add(Contact p);
+        void Update(Contact p);
+        void Delete(Contact p);
+
+        Contact Find(int id);
+    }
+}
