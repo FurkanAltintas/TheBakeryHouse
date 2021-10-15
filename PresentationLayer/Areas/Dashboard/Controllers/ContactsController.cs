58016d50ca406a579b5f0517d350315940632c41
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:56:25 +0300

    Contacts Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/ContactsController.cs
@@ -0,0 +1,29 @@
+﻿    using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class ContactsController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        // GET: Dashboard/Contacts
+        public ActionResult Index()
+        {
+            var list = contactManager.List();
+            return View(list);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var key = contactManager.Find(id);
+            contactManager.Delete(key);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
