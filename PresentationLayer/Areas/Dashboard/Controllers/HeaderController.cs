4183fd7d6ba199c03e3f329d0101a5d9cc49ea35
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:56:53 +0300

    Header Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/HeaderController.cs
@@ -0,0 +1,34 @@
+﻿using EntityLayer.Concrete;
+using EntityLayer.Dto;
+using PresentationLayer.Areas.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class HeaderController : BaseController
+    {
+        Users users = new Users();
+        // GET: Dashboard/Header
+        public PartialViewResult Profile()
+        {
+            return PartialView(User());
+        }
+
+        public PartialViewResult Navbar()
+        {
+            return PartialView(User());
+        }
+
+        public Users User()
+        {
+            users.FullName = Name;
+            users.Profile = Image;
+            users.Email = Email;
+            return users;
+        }
+    }
+}
