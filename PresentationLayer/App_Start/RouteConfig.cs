9de9dc1f5b2b19d57a99bd6e725b06c81a4e4994
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 20:26:15 +0300

    PresentationLayer

--- PresentationLayer/App_Start/RouteConfig.cs
+++ PresentationLayer/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace PresentationLayer
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "PresentationLayer.Controllers" }
             );
         }
     }
