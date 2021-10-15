242185d1908af4f73773db30cdaec4a029747db6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:57:23 +0300

    Panel Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/PanelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class PanelController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            string actionName = this.ControllerContext.RouteData.Values["action"].ToString();
+            string controllerName = this.ControllerContext.RouteData.Values["controller"].ToString();
+
+            return View();
+        }
+    }
+}
