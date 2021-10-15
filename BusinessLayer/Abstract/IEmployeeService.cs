82f19818b423bfc0d1264db41bd637a5643bff71
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:45:54 +0300

    Employee Service

---
+++ BusinessLayer/Abstract/IEmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IEmployeeService
+    {
+        List<Employee> List();
+        void Add(Employee p);
+        void Update(Employee p);
+        void Delete(Employee p);
+
+        Employee Find(string email);
+        bool Login(Employee p);
+    }
+}
