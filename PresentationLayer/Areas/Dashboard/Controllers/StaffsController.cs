e409fe28e32af14456d5a1970d3433cfb934e5d6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:57:54 +0300

    Staffs Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/StaffsController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class StaffsController : Controller
+    {
+        StaffManager staffManager = new StaffManager(new EfStaffDal());
+        // GET: Dashboard/Staffs
+        public ActionResult Index()
+        {
+            var list = staffManager.List();
+            return View(list);
+        }
+    }
+}
