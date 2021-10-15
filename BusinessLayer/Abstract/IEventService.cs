eddee761b1cc6fb1a45946f6bb047cd5a5f6b7fb
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:46:04 +0300

    Event Service

---
+++ BusinessLayer/Abstract/IEventService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IEventService
+    {
+        List<Event> List();
+        void Add(Event p);
+        void Update(Event p);
+        void Delete(Event p);
+
+        Event Find(int id);
+    }
+}
