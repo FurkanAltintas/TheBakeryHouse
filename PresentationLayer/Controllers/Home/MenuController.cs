2f9b99820a32fce6cb8a061a1ab28de82a22a19f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 17:32:40 +0300

    Menu Controller

---
+++ PresentationLayer/Controllers/Home/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers.Home
+{
+    public class MenuController : Controller
+    {
+        MenuManager menuManager = new MenuManager(new EfMenuDal());
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult List()
+        {
+            var list = menuManager.List();
+            return PartialView(list);
+        }
+
+        public PartialViewResult TopRated()
+        {
+            var list = menuManager.ListTopRated();
+            return PartialView(list);
+        }
+    }
+}
