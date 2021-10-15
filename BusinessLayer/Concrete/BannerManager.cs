338a22f92b4128491ef152d972229c589a377a9d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:47:17 +0300

    Banner Manager

---
+++ BusinessLayer/Concrete/BannerManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BannerManager : IBannerService
+    {
+        IBannerDal _bannerDal;
+
+        public BannerManager(IBannerDal bannerDal)
+        {
+            _bannerDal = bannerDal;
+        }
+
+        public void Add(Banner p)
+        {
+            _bannerDal.Create(p);
+        }
+
+        public void Delete(Banner p)
+        {
+            _bannerDal.Delete(p);
+        }
+
+        public Banner First()
+        {
+            return _bannerDal.First();
+        }
+
+        public List<Banner> List()
+        {
+            return _bannerDal.Read();
+        }
+
+        public void Update(Banner p)
+        {
+            _bannerDal.Update(p);
+        }
+    }
+}
