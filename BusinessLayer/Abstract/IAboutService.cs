67fd3a973453001e8e8af85cfe87011889bbc318
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:45:09 +0300

    About Service

---
+++ BusinessLayer/Abstract/IAboutService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IAboutService
+    {
+        List<About> List();
+        void Add(About p);
+        void Update(About p);
+        void Delete(About p);
+        About First();
+    }
+}
