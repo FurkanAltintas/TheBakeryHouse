d0c56f634d0505071d527cbdb3b69dd53410ae3b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:56:33 +0300

    Employee Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeManager employeeManager = new EmployeeManager(new EfEmployeeDal());
+        // GET: Dashboard/Employee
+        public ActionResult Index()
+        {
+            var list = employeeManager.List();
+            return View(list);
+        }
+    }
+}
