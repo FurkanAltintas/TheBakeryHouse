405b369e30aeff8355dbd9437902c391cdad36af
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:46:29 +0300

    Menu Service

---
+++ BusinessLayer/Abstract/IMenuService.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IMenuService
+    {
+        List<Menu> List();
+        List<Menu> ListTopRated();
+        void Add(Menu p);
+        void Update(Menu p);
+        void Delete(Menu p);
+
+        Menu Find(int id);
+
+        List<Currency> ListCurrency();
+    }
+}
