49d130fda7650639a8f236b8a52caaf8844ef176
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:56:07 +0300

    Account Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class AccountController : Controller
+    {
+        EmployeeManager employeeManager = new EmployeeManager(new EfEmployeeDal());
+        // GET: Dashboard/Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Employee p)
+        {
+            if (employeeManager.Login(p))
+            {
+                FormsAuthentication.SetAuthCookie(p.Email, false);
+                Session["Email"] = p.Email;
+                return RedirectToAction("Index", "Panel");
+            }
+            else
+            {
+                ViewBag.Error = "Your email or password is incorrect";
+                return View(p);
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home", new { Area = "" });
+        }
+    }
+}
