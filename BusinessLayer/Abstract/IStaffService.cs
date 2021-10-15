479d4274bc6d6751b4a91f2bfefe9949aa1dcecb
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:46:43 +0300

    Staff Service

---
+++ BusinessLayer/Abstract/IStaffService.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IStaffService
+    {
+        List<Staff> List();
+        void Add(Staff p);
+        void Update(Staff p);
+        void Delete(Staff p);
+    }
+}
