fd4979854bc32e9d6f74949bfce3738385f8afa9
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 20:20:48 +0300

    Banner

---
+++ EntityLayer/Concrete/Banner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Banner
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Url { get; set; }
+        public string Button { get; set; }
+    }
+}
