5670afd84b9550bbd19594c9f40693107086b009
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 17:33:15 +0300

    Gallery Controller

---
+++ PresentationLayer/Controllers/Home/GalleryController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers.Home
+{
+    public class GalleryController : Controller
+    {
+        GalleryManager galleryManager = new GalleryManager(new EfGalleryDal());
+        // GET: _PartialGallery
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult List()
+        {
+            var list = galleryManager.List();
+            return PartialView(list);
+        }
+    }
+}
