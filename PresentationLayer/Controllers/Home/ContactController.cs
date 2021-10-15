4bb0d17d87ed31a931e8f1f956af7bb706be1912
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 17:34:41 +0300

    Contact Controller

---
+++ PresentationLayer/Controllers/Home/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers.Home
+{
+    public class ContactController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.Success = TempData["Added"] as string;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Contact p)
+        {
+            p.DateTime = DateTime.Now;
+            contactManager.Add(p);
+            TempData["Added"] = "Added";
+            return RedirectToAction("Index");
+        }
+    }
+}
