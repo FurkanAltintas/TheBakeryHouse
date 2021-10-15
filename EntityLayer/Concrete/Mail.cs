f34014d5a3d0ccef3c7de411e4fa1199432a6360
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 20:21:27 +0300

    Mail

---
+++ EntityLayer/Concrete/Mail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Mail
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Recipient { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public string Attachments { get; set; }
+        public bool IsDraft { get; set; } //draft
+        public bool IsStar { get; set; } //star
+        public bool IsRead { get; set; } //read
+        public bool IsTrash { get; set; } //trash
+        public DateTime History { get; set; }
+    }
+}
