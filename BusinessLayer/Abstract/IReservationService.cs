e6e3bc27aab475b45e140b3ed6bc2638235f7537
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:46:36 +0300

    Reservation Service

---
+++ BusinessLayer/Abstract/IReservationService.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using EntityLayer.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IReservationService
+    {
+        List<Reservation> List();
+        void Add(Reservation p);
+        void Update(Reservation p);
+        void Delete(Reservation p);
+
+        Reservation Find(int id);
+
+        List<Calendar> ListCalendar();
+    }
+}
