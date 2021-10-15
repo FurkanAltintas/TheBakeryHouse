229552e1404fc2ec90601fe702ff35b14d97463b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:46:21 +0300

    Mail Service

---
+++ BusinessLayer/Abstract/IMailService.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IMailService
+    {
+        List<Mail> Inbox(string p=null, string addition = null);
+        IEnumerable<Mail> SendBox(string mail);
+
+        void Add(Mail p);
+        void Update(Mail p);
+        void Delete(Mail p);
+
+        Mail Find(int id);
+    }
+}
