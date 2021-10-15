107bb5afce7fe4981011e8729114ca7c92913337
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:49:54 +0300

    IRepository

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T> where T : class
+    {
+        //CRUD --> Create - Read - Update -Delete
+        void Create(T p);
+        List<T> Read(Expression<Func<T, bool>> filter = null);
+        void Update(T p);
+        void Delete(T p);
+        T Get(Expression<Func<T, bool>> filter);
+    }
+}
