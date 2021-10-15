2997ba9d5c320453f3067462ed47a858aedb0e7b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 17:32:05 +0300

    Staff Controller

---
+++ PresentationLayer/Controllers/Home/StaffController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers.Home
+{
+    public class StaffController : Controller
+    {
+        StaffManager staffManager = new StaffManager(new EfStaffDal());
+        // GET: _PartialStaff
+        public PartialViewResult Index()
+        {
+            var list = staffManager.List();
+            return PartialView(list);
+        }
+    }
+}
