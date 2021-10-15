80d58406110582c57aefc0edd9ea440225c2b54f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:45:28 +0300

    Banner Service

---
+++ BusinessLayer/Abstract/IBannerService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IBannerService
+    {
+        List<Banner> List();
+        void Add(Banner p);
+        void Update(Banner p);
+        void Delete(Banner p);
+        Banner First();
+    }
+}
