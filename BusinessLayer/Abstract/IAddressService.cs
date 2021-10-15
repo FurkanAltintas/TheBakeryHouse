e9f01e5783a9f06c403d201115967375ce60da76
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 19:45:17 +0300

    Address Service

---
+++ BusinessLayer/Abstract/IAddressService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IAddressService
+    {
+        List<Address> List();
+        void Add(Address p);
+        void Update(Address p);
+        void Delete(Address p);
+        Address First();
+    }
+}
