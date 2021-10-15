805908ce43fa9e6be5d848344037c56aa4f34aed
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-15 20:21:38 +0300

    Reservation

---
+++ EntityLayer/Concrete/Reservation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Reservation : BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        [ForeignKey("EventId")]
+        public Event Event { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Message { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
