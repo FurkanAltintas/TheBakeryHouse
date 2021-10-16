b9433fcfbce761e57f654e5245652f21c9c016f1
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-16 17:20:51 +0300

    Gallery Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class GalleryController : Controller
+    {
+        GalleryManager galleryManager = new GalleryManager(new EfGalleryDal());
+        // GET: Dashboard/Gallery
+        public ActionResult Index()
+        {
+            return View(galleryManager.List());
+        }
+    }
+}
