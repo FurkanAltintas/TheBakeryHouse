2c09949e283c64f38ad11cfad3ee9635630ab065
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-16 17:23:04 +0300

    Authority Manager

---
+++ BusinessLayer/Concrete/AuthorityManager.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AuthorityManager : IAuthorityService
+    {
+        IAuthorityDal _authorityDal;
+
+        public AuthorityManager(IAuthorityDal authorityDal)
+        {
+            _authorityDal = authorityDal;
+        }
+
+        public void Add(Authority p)
+        {
+            _authorityDal.Create(p);
+        }
+
+        public void Delete(Authority p)
+        {
+            _authorityDal.Delete(p);
+        }
+
+        public List<Authority> List()
+        {
+            return _authorityDal.Read();
+        }
+
+        public void Update(Authority p)
+        {
+            _authorityDal.Update(p);
+        }
+    }
+}
