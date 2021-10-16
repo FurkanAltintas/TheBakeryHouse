702a87423d8071c7467b3653e2fc752228f4546c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-16 17:21:19 +0300

    Authority Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/AuthorityController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class AuthorityController : Controller
+    {
+        AuthorityManager authorityManager = new AuthorityManager(new EfAuthorityDal());
+        // GET: Dashboard/Authority
+        public ActionResult Index()
+        {
+            return View(authorityManager.List());
+        }
+    }
+}
