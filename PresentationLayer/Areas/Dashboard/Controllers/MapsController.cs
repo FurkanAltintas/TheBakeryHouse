cff082cf58d845e65b58424e7ff21744c50ec285
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-16 18:10:32 +0300

    Maps Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/MapsController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class MapsController : Controller
+    {
+        AddressManager addressManager = new AddressManager(new EfAddressDal());
+        // GET: Dashboard/Maps
+        public ActionResult Index()
+        {
+            return View(addressManager.First());
+        }
+    }
+}
