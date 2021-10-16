4d62ca45a761d0b9491765bab05018d3f960b795
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-16 17:54:30 +0300

    Banners Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/BannersController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class BannersController : Controller
+    {
+        BannerManager bannerManager = new BannerManager(new EfBannerDal());
+        // GET: Dashboard/Banner
+        public ActionResult Index()
+        {
+            return View(bannerManager.First());
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Banner p)
+        {
+            bannerManager.Update(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
