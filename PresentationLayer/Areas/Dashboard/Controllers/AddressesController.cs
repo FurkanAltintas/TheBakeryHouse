ee7a8df41b854a43b8202287681f9dd244d063e6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-16 18:10:42 +0300

    Addresses Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/AddressesController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class AddressesController : Controller
+    {
+        AddressManager addressManager = new AddressManager(new EfAddressDal());
+        // GET: Dashboard/Address
+        public ActionResult Index()
+        {
+            return View(addressManager.First());
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Address p)
+        {
+            addressManager.Update(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
