bd273a80a630378beec9f3b581e4295a202d2162
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-16 17:21:51 +0300

    About Controller

---
+++ PresentationLayer/Areas/Dashboard/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Areas.Dashboard.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+        // GET: Dashboard/About
+        public ActionResult Index()
+        {
+            return View(aboutManager.First());
+        }
+
+        [HttpPost]
+        public ActionResult Edit(About p)
+        {
+            aboutManager.Update(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
