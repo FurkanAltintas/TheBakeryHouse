592c2d3c30d00a3bfbd5b7e1c3fa3b83024e4b6c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-16 17:23:12 +0300

    Authority Service

---
+++ BusinessLayer/Abstract/IAuthorityService.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IAuthorityService
+    {
+        List<Authority> List();
+        void Add(Authority p);
+        void Update(Authority p);
+        void Delete(Authority p);
+    }
+}
