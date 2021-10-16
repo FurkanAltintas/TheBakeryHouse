0c578d7d1f171f4f3c320cf8d4756a65b0b48531
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-16 16:21:38 +0300

    Generic Repository

--- DataAccessLayer/Repository/GenericRepository.cs
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -12,53 +12,45 @@ namespace DataAccessLayer.Repository
 {
     public class GenericRepository<T> : IRepository<T> where T : class
     {
+        private Context c = new Context();
+        private readonly DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = c.Set<T>();
+        }
         public void Create(T p)
         {
-            using (var db = new Context())
-            {
-                db.Entry(p).State = EntityState.Added;
-                db.SaveChanges();
-            }
+            c.Entry(p).State = EntityState.Added;
+            c.SaveChanges();
         }
 
         public void Delete(T p)
         {
-            using (var db = new Context())
-            {
-                db.Entry(p).State = EntityState.Deleted;
-            }
+            c.Entry(p).State = EntityState.Deleted;
         }
 
         public T Get(Expression<Func<T, bool>> filter)
         {
-            using (var db = new Context())
-            {
-                return db.Set<T>().SingleOrDefault(filter);
-            }
+            return _object.SingleOrDefault(filter);
         }
 
         public List<T> Read(Expression<Func<T, bool>> filter = null)
         {
-            using (var db = new Context())
+            if (filter != null)
             {
-                if (filter != null)
-                {
-                    return db.Set<T>().Where(filter).ToList();
-                }
-                else
-                {
-                    return db.Set<T>().ToList();
-                }
+                return _object.Where(filter).ToList();
+            }
+            else
+            {
+                return _object.ToList();
             }
         }
 
         public void Update(T p)
         {
-            using (var db = new Context())
-            {
-                db.Entry(p).State = EntityState.Modified;
-                db.SaveChanges();
-            }
+            c.Entry(p).State = EntityState.Modified;
+            c.SaveChanges();
         }
     }
 }
