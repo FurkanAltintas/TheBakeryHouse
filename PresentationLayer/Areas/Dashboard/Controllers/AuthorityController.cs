fd125fb8bb3ce13f5e13a9bbc33170c27818b78b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-16 17:54:08 +0300

    Authority Controller

--- PresentationLayer/Areas/Dashboard/Controllers/AuthorityController.cs
+++ PresentationLayer/Areas/Dashboard/Controllers/AuthorityController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,41 @@ namespace PresentationLayer.Areas.Dashboard.Controllers
         public ActionResult Index()
         {
             return View(authorityManager.List());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Authority p)
+        {
+            if (p.Id > 0)
+                authorityManager.Update(p);
+            {
+                authorityManager.Add(p);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var key = Find(id);
+            authorityManager.Delete(key);
+            return RedirectToAction("Index");
+        }
+
+        public Authority Find(int id)
+        {
+            return authorityManager.Find(id);
         }
     }
 }
